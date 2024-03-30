fcd4f3612f7d29819f2f46181e4cea2249c2a059
Valeriia Kropyvnytska <v.kropyvnytska@apriorit.com>
2024-03-30 21:20:18 +0200

    Added function docstrings

--- app/io/input.py
+++ app/io/input.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
 def input_text_from_console():
-
+    """
+    Gets input from console.
+    """
 
 def read_text_from_file(file_path):
+    """
+    Gets input from text file.
 
+    Args:
+    :arg file_path - path to input file
+    """
 
 def read_text_from_file_with_pandas(file_path):
+    """
+    Gets input from text file using pandas module.
 
+    Args:
+    :arg file_path - path to input file
+    """
 
--- app/io/output.py
+++ app/io/output.py
@@ -1,5 +1,16 @@
-def output_text_to_console():
+def output_text_to_console(text):
+    """
+    Outputs text to console.
 
+    Args:
+    :arg text - text to output
+    """
 
-def output_text_to_file(file_path):
+def output_text_to_file(text, file_path):
+    """
+    Outputs text to file.
 
+    Args:
+    :arg text - text to output
+    :arg file_path - path to output file
+    """
